6f7f02131d2e8ac88d5d723eebd0266d50dadca5
ValentinMinder <valentin.minder@epfl.ch>
2015-03-31 00:18:52 +0200

    V1client impl compliant with furnished tests

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -16,53 +16,117 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import java.util.LinkedList;
+
 /**
  * This class implements the client side of the protocol specification (version 1).
  * 
- * @author Olivier Liechti
+ * @author Olivier Liechti, Valentin Minder
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
   private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
 
+  // socket for server connection
+  private Socket mysocket = null;
+  // input from server
+  private BufferedReader br = null;
+  // output to server (dont forget to flush before expecting response!)
+  private PrintWriter pw = null;
+  // welcome message to avoid...
+  private final String welcome = "Hello. Online HELP is available. Will you find it?";
+  
+  
+  private String myReadLine() throws IOException {
+      String line;
+      do {
+          line = br.readLine();
+      } while (line.equalsIgnoreCase(welcome));
+      return line;
+  }
+  
   @Override
   public void connect(String server, int port) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      mysocket = new Socket(server, port);
+      pw = new PrintWriter(new OutputStreamWriter(mysocket.getOutputStream()));
+      br = new BufferedReader(new InputStreamReader(mysocket.getInputStream()));
+      //br.readLine(); // welcome message!
   }
 
   @Override
   public void disconnect() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      pw.close();
+      br.close();
+      mysocket.close();
   }
 
   @Override
   public boolean isConnected() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      if (mysocket == null) {
+         return false; 
+      } else {
+          return mysocket.isConnected() && !mysocket.isClosed();
+      } // git trick for braces
   }
 
   @Override
   public void loadStudent(String fullname) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      List<Student> list = new LinkedList<Student>();
+      list.add(new Student(fullname));
+      loadStudents(list);
   }
 
   @Override
   public void loadStudents(List<Student> students) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      pw.write("LOAD\n");
+      pw.flush();
+
+      String waitStart = "Send your data [end with ENDOFDATA]";
+      if (!myReadLine().equalsIgnoreCase(waitStart)) {
+          throw new IOException("server response not correct....");
+      } // git trick for braces
+
+      for (Student student : students) {
+          pw.write(student.getFullname()+"\n");
+      } // git trick for braces
+
+      pw.write("ENDOFDATA\n");
+      pw.flush();
+
+      String waitEnd = "DATA LOADED";
+      if (!myReadLine().equalsIgnoreCase(waitEnd)) {
+          throw new IOException("server response not correct....");
+      } // git trick for braces
   }
 
   @Override
   public Student pickRandomStudent() throws EmptyStoreException, IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      pw.write("RANDOM\n");
+      pw.flush();
+
+      RandomCommandResponse rcr = JsonObjectMapper.parseJson(myReadLine(), RandomCommandResponse.class);
+      if(rcr.getError() != null) {
+          throw new EmptyStoreException();
+      } // git trick for braces
+      return new Student(rcr.getFullname());
   }
 
   @Override
   public int getNumberOfStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      pw.write("INFO\n");
+      pw.flush();
+
+      InfoCommandResponse icr = JsonObjectMapper.parseJson(myReadLine(), InfoCommandResponse.class);
+      return icr.getNumberOfStudents();
   }
 
   @Override
   public String getProtocolVersion() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      pw.write("INFO\n");
+      pw.flush();
+
+      InfoCommandResponse icr = JsonObjectMapper.parseJson(myReadLine(), InfoCommandResponse.class);
+      return icr.getProtocolVersion();
   }
 
 
