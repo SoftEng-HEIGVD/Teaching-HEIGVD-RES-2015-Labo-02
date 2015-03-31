70438c2c501798be0428866b10c29eda0822fc52
paulnta <paul.ntawuruhunga@heig-vd.ch>
2015-03-31 10:09:26 +0200

    deleted wrong directory

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2PaulntaTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2PaulntaTest.java
@@ -1,7 +1,6 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.data.Student;
-import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import ch.heigvd.schoolpulse.TestAuthor;
 import org.junit.Rule;
@@ -9,8 +8,10 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.IOException;
-import static org.junit.Assert.assertEquals;
+import java.util.LinkedList;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -30,6 +31,32 @@ public class RouletteV2PaulntaTest {
 
     @Test
     @TestAuthor(githubId = {"wasadigi", "SoftEng-HEIGVD"})
+    public void theServerShouldClearDataCorrectly() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        client.loadStudent("Paul");
+        client.loadStudent("Karim");
+        assertEquals(2, client.getNumberOfStudents());
+        client.clearDataStore();
+        assertEquals(0, client.getNumberOfStudents());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"wasadigi", "SoftEng-HEIGVD"})
+    public void theServerShouldKeepStudentsAfterADisconnection() throws IOException {
+        IRouletteV2Client client = (IRouletteV2Client)roulettePair.getClient();
+        client.loadStudent("Paul Walker");
+        client.loadStudent("Paul McCartney");
+        client.loadStudent("Paul Nta");
+        client.disconnect();
+
+        IRouletteV2Client client2 = new RouletteV2ClientImpl();
+        client2.connect("localhost", roulettePair.getServer().getPort());
+        assertEquals(3, client2.getNumberOfStudents());
+    }
+
+
+    @Test
+    @TestAuthor(githubId = {"wasadigi", "SoftEng-HEIGVD"})
     public void theServerShouldManageTwoCLients() throws IOException {
         int port = roulettePair.getServer().getPort();
         IRouletteV1Client client1 = new RouletteV1ClientImpl();
@@ -40,35 +67,38 @@ public class RouletteV2PaulntaTest {
         assertTrue(client2.isConnected());
     }
 
-  @Test
-  @TestAuthor(githubId = {"gweezer7", "paulnta"})
-  public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
-    assertEquals(RouletteV1Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
-  }
-  @Test
-  @TestAuthor(githubId = {"gweezer7", "paulnta"})
-  public void theServerShouldFetchAStudentInTheList() throws IOException {
-      IRouletteV2Client client =(IRouletteV2Client) roulettePair.getClient();
-      Student jack = new Student("Jacques");
-      client.loadStudent(jack.getFullname());
-      assertTrue(client.listStudents().contains(jack));
-  }
-    
-  @Test
-  @TestAuthor(githubId = {"gweezer7", "paulnta"})
-  public void theSizeOfTheListMustBeEqualToTheNumberOfClint() throws IOException {
-      IRouletteV2Client client =(IRouletteV2Client) roulettePair.getClient();      
-      assertTrue(client.listStudents().isEmpty());
-  }
+    @Test
+    @TestAuthor(githubId = {"gweezer7", "paulnta"})
+    public void theServerShouldReturnTheCorrectVersionNumber() throws IOException {
+        assertEquals(RouletteV2Protocol.VERSION, roulettePair.getClient().getProtocolVersion());
+    }
+    @Test
+    @TestAuthor(githubId = {"gweezer7", "paulnta"})
+    public void theServerShouldFetchAStudentInTheList() throws IOException {
+        IRouletteV2Client client =(IRouletteV2Client) roulettePair.getClient();
+        Student jack = new Student("Jacques");
+        client.loadStudent(jack.getFullname());
+        assertTrue(client.listStudents().contains(jack));
+    }
 
-  
-  @Test
-  @TestAuthor(githubId = {"gweezer7", "paulnta"})
-  public void theSizeOfTheListMustBequalToTheNumberOfClient() throws IOException {
-      IRouletteV2Client client =(IRouletteV2Client) roulettePair.getClient();      
-      client.loadStudent("Jacques");
-      assertTrue(client.listStudents().size() == 1);
-      client.loadStudent("Josiane");
-      assertTrue(client.listStudents().size() == 2);
-  }
+    @Test
+    @TestAuthor(githubId = {"gweezer7", "paulnta"})
+    public void theSizeOfTheListMustBeEqualToTheNumberOfClint() throws IOException {
+        IRouletteV2Client client =(IRouletteV2Client) roulettePair.getClient();
+        assertTrue(client.listStudents().isEmpty());
+    }
+
+
+    @Test
+    @TestAuthor(githubId = {"gweezer7", "paulnta"})
+    public void theSizeOfTheListMustBequalToTheNumberOfClient() throws IOException {
+        IRouletteV2Client client =(IRouletteV2Client) roulettePair.getClient();
+        client.loadStudent("Jacques");
+        assertTrue(client.listStudents().size() == 1);
+        client.loadStudent("Josiane");
+        assertTrue(client.listStudents().size() == 2);
+    }
+
+
+
 }
