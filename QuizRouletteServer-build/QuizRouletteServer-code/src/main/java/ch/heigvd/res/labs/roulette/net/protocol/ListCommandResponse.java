d001bae75cc2474ad5c2564f3b14ce856a8d249e
guiguismall <guillaume.bruge@heig-vd.ch>
2015-04-12 16:55:46 +0200

    new response

---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/ListCommandResponse.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+import ch.heigvd.res.labs.roulette.data.Student;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author Guillaume
+ */
+public class ListCommandResponse {
+    private List<Student> students = new LinkedList();
+    
+    public ListCommandResponse() {
+        
+    }
+    
+    public ListCommandResponse(List<Student> students) {
+        this.students = students;
+    }
+    
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+    
+    public List<Student> getStudents() {
+        return students;
+    }
+}
