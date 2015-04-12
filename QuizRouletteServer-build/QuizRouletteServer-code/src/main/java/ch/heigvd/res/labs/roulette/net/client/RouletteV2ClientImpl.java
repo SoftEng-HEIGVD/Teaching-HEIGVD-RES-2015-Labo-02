cdf1cb91141fdae61911d3ac0c4d50c49633d877
David Kunzmann <davidkunzmann@gmail.com>
2015-04-12 12:16:28 +0200

    V2 Ok

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -5,7 +5,9 @@ import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
+import javax.imageio.IIOException;
 
 /**
  * This class implements the client side of the protocol specification (version 2).
@@ -16,12 +18,21 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
 
   @Override
   public void clearDataStore() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      writer.println(RouletteV2Protocol.CMD_CLEAR);
+      writer.flush();
+      String line;
+      line=lineReader();
+      if(!line.equalsIgnoreCase(RouletteV2Protocol.RESPONSE_CLEAR_DONE)){
+          throw new IOException("server response not correct....");
+      }
   }
 
   @Override
   public List<Student> listStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      writer.println(RouletteV2Protocol.CMD_LIST);
+      writer.flush();
+      StudentsList s = JsonObjectMapper.parseJson(lineReader(), StudentsList.class);
+      return s.getStudents();
   }
   
 }
