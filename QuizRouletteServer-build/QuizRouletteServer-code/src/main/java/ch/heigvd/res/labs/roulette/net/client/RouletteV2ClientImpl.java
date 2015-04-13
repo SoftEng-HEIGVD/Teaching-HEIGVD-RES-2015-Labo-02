92165d9572b407880ad12ee8faa436c407a28157
David Kunzmann <davidkunzmann@gmail.com>
2015-04-13 20:28:59 +0200

    Test can be executed

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -4,6 +4,7 @@ import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.ByeCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.LoadCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
 import javax.imageio.IIOException;
 
 /**
@@ -19,14 +21,17 @@ import javax.imageio.IIOException;
  * @author Olivier Liechti
  */
 public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRouletteV2Client {
-
+    
+    private static final java.util.logging.Logger LOG = java.util.logging.Logger.getLogger(RouletteV2ClientImpl.class.getName());
+  
+    
+    
   @Override
   public void clearDataStore() throws IOException {
       writer.println(RouletteV2Protocol.CMD_CLEAR);
       writer.flush();
-      String line;
-      line=lineReader();
-      if(!line.equalsIgnoreCase(RouletteV2Protocol.RESPONSE_CLEAR_DONE)){
+
+      if(!lineReader().equalsIgnoreCase(RouletteV2Protocol.RESPONSE_CLEAR_DONE)){
           throw new IOException("server response not correct....");
       }
       
@@ -36,48 +41,20 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
   public List<Student> listStudents() throws IOException {
       writer.println(RouletteV2Protocol.CMD_LIST);
       writer.flush();
-      StudentsList s = JsonObjectMapper.parseJson(lineReader(), StudentsList.class);
+      String line = lineReader();
+      StudentsList s = JsonObjectMapper.parseJson(line, StudentsList.class);
       return s.getStudents();
   }
   
-  @Override
-  public void disconnect() throws IOException{
-      String line = lineReader();
-      ByeCommandResponse bcr = JsonObjectMapper.parseJson(line, ByeCommandResponse.class);
-      
-      if(!bcr.getStatus().equalsIgnoreCase("success")){
-          throw new IOException("Operation failure!");
-      }
-  }
-  
-    @Override
-  public void loadStudents(List<Student> students) throws IOException {
-      writer.println(RouletteV1Protocol.CMD_LOAD);
-      writer.flush();
-      String line;
-      line=lineReader();
-      if(!line.equalsIgnoreCase(RouletteV1Protocol.RESPONSE_LOAD_START)){
-          throw new IOException("server response not correct....");
-      }
-      System.out.println(line);
-      Iterator<Student> s = students.iterator();
-      while(s.hasNext()){
-          writer.println(s.next().getFullname());
-          writer.flush();
-      }
-      writer.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
-      writer.flush();
-      line=lineReader();
-      LoadCommandResponse lcr = JsonObjectMapper.parseJson(line, LoadCommandResponse.class);
-      System.out.println("Gnarf");
-      if(!lcr.getStatus().equalsIgnoreCase("success")){
-          throw new IOException("Operation failure!");
-      }
-      line=lineReader();
-      if(!line.equalsIgnoreCase(RouletteV1Protocol.RESPONSE_LOAD_DONE)){
-          throw new IOException("server response not correct....");
-      }
-  }
+
+//    protected void endLoad() throws IOException {
+//      LoadCommandResponse lcs = JsonObjectMapper.parseJson(lineReader(), LoadCommandResponse.class);
+//      if (lcs.getStatus().equalsIgnoreCase("success")) {
+//          LOG.log(Level.INFO, "Added successfully: {0} students", lcs.getNbStudents());
+//      } else {
+//          LOG.severe("Error. Students not added...");
+//      } 
+//  }
   
 }
 
