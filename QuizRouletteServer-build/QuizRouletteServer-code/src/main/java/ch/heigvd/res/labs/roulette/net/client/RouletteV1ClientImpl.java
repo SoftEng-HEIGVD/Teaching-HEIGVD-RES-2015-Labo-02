715d2eba15b1fd00d2b65e672be93f3c1f69b2e6
Manamiz <thibduch@hotmail.com>
2015-04-13 07:58:40 +0200

    Labo 2 full implementation

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -13,7 +13,6 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -25,27 +24,39 @@ import java.util.logging.Logger;
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
     private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
-    
-    private BufferedReader bufferedReader;
-    private PrintWriter printWriter;
+
+    protected BufferedReader bufferedReader;
+    protected PrintWriter printWriter;
     private Socket socket;
 
     @Override
     public void connect(String server, int port) throws IOException {
+        // On instancie les champs
         socket = new Socket(server, port);
         bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-        
+
+        // On lit le message de bienvenue
         bufferedReader.readLine();
     }
 
     @Override
     public void disconnect() throws IOException {
-        printWriter.println(RouletteV1Protocol.CMD_BYE);
-        
-        printWriter.close();
-        bufferedReader.close();
-        socket.close();
+        if (isConnected()) {
+            printAndFlush(RouletteV1Protocol.CMD_BYE);
+
+            // On ferme les différents flux ouverts et le socket
+            printWriter.close();
+            bufferedReader.close();
+            socket.close();
+            
+            // On les met à null
+            printWriter = null;
+            bufferedReader = null;
+            socket = null;
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
     }
 
     @Override
@@ -55,27 +66,92 @@ public class RouletteV1ClientImpl implements IRouletteV1Client {
 
     @Override
     public void loadStudent(String fullname) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isConnected()) {
+            printAndFlush(RouletteV1Protocol.CMD_LOAD);
+
+            bufferedReader.readLine();
+
+            printAndFlush(fullname);
+
+            printAndFlush(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+
+            bufferedReader.readLine();
+
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
     }
 
     @Override
     public void loadStudents(List<Student> students) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isConnected()) {
+            printAndFlush(RouletteV1Protocol.CMD_LOAD);
+
+            bufferedReader.readLine();
+
+            for (Student s : students) {
+                printWriter.println(s.getFullname());
+            }
+
+            printAndFlush(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+
+            bufferedReader.readLine();
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
     }
 
     @Override
     public Student pickRandomStudent() throws EmptyStoreException, IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isConnected()) {
+            printAndFlush(RouletteV1Protocol.CMD_RANDOM);
+
+            // On convertit le string en RandomCommandResponse (déserialise)
+            RandomCommandResponse randomCommandResponse = JsonObjectMapper.parseJson(bufferedReader.readLine(), RandomCommandResponse.class);
+
+            // Si l'on a une erreur, on lève une exception
+            if (randomCommandResponse.getError() != null) {
+                throw new EmptyStoreException();
+            }
+
+            return new Student(randomCommandResponse.getFullname());
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
     }
 
     @Override
     public int getNumberOfStudents() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isConnected()) {
+            printAndFlush(RouletteV1Protocol.CMD_INFO);
+
+            // On convertit le string en InfoCommandResponse (déserialise)
+            InfoCommandResponse infoCommandResponse = JsonObjectMapper.parseJson(bufferedReader.readLine(), InfoCommandResponse.class);
+
+            return infoCommandResponse.getNumberOfStudents();
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
     }
 
     @Override
     public String getProtocolVersion() throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isConnected()) {
+            printAndFlush(RouletteV1Protocol.CMD_INFO);
+
+            // On convertit le string en InfoCommandResponse (déserialise)
+            InfoCommandResponse infoCommandResponse = JsonObjectMapper.parseJson(bufferedReader.readLine(), InfoCommandResponse.class);
+
+            return infoCommandResponse.getProtocolVersion();
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
+    }
+
+    // Appelle la méthode println(text) et flush() du PrintWriter de la classe
+    protected void printAndFlush(String text) {
+        printWriter.println(text);
+        printWriter.flush();
     }
 
 }
--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -8,20 +8,37 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- * This class implements the client side of the protocol specification (version 2).
+ * This class implements the client side of the protocol specification (version
+ * 2).
  *
- * @author Olivier Liechti
+ * @author Olivier Liechti, Thibaud Duchoud, Mario Ferreira
  */
 public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRouletteV2Client {
 
-  @Override
-  public void clearDataStore() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void clearDataStore() throws IOException {
+        if (isConnected()) {
+            printAndFlush(RouletteV2Protocol.CMD_CLEAR);
 
-  @Override
-  public List<Student> listStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-  
+                bufferedReader.readLine();
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
+    }
+
+    @Override
+    public List<Student> listStudents() throws IOException {
+        if (isConnected()) {
+            printAndFlush(RouletteV2Protocol.CMD_LIST);
+
+            // On convertit le string en StudentsList (déserialise)
+            StudentsList studentsList = JsonObjectMapper.parseJson(bufferedReader.readLine(), StudentsList.class);
+
+            // On retourne la List<Student>
+            return studentsList.getStudents();
+        } else {
+            throw new IOException("Connection error : socket not connected");
+        }
+    }
+
 }
