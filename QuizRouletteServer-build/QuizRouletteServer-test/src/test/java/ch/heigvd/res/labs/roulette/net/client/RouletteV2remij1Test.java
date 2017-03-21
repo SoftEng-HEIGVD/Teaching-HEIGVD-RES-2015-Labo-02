e1e7de6c8ab2fb01fb8c665f67971b8d5ed72b5d
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-03-21 17:49:11 +0100

    Init testV2

---
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2remij1Test.java
@@ -0,0 +1,15 @@
+package ch.heigvd.res.labs.roulette.net.client;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Remi
+ */
+public class RouletteV2remij1Test {
+    
+}
