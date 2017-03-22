c982e9ee4b93089144cfcdce3eeb2343a9ce073f
Newtt <m.chatelan@gmail.com>
2017-03-22 17:13:40 +0100

    All tests are in green, need code checking and reviewing

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -4,10 +4,12 @@ import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
 
 import java.io.*;
 import java.net.Socket;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 /**
@@ -29,6 +31,9 @@ public class RouletteV1ClientImpl implements IRouletteV1Client
       socket = new Socket(server, port);
       reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+      // We get the hello message from the server (we don't use it)
+      reader.readLine();
    }
 
    @Override
@@ -50,13 +55,11 @@ public class RouletteV1ClientImpl implements IRouletteV1Client
    @Override
    public void loadStudent(String fullname) throws IOException
    {
-      //TODO Fix de cette requête au serveur
-      reader.readLine();
-
       // Ask the server the current version
       writer.println("LOAD");
       writer.flush();
 
+      // Trash the reply from the server (we know how to talk to the server)
       reader.readLine();
 
       // Load the student given in parameters
@@ -79,16 +82,24 @@ public class RouletteV1ClientImpl implements IRouletteV1Client
    @Override
    public Student pickRandomStudent() throws EmptyStoreException, IOException
    {
-      //TODO Implémenter la classe
-      throw new EmptyStoreException(); //To change body of generated methods, choose Tools | Templates.
+      // Ask the server to provice a random student
+      writer.println("RANDOM");
+      writer.flush();
+
+      // We collect the reply
+      RandomCommandResponse response = JsonObjectMapper.parseJson(reader.readLine(), RandomCommandResponse.class);
+
+      // If there is no error, return a new Student with the name received
+      if (response.getError().equals(""))
+         return new Student(response.getFullname());
+      // Otherwise if there is an error, throw an exception
+      else
+         throw new EmptyStoreException();
    }
 
    @Override
    public int getNumberOfStudents() throws IOException
    {
-      // Read the hello message from the server
-      reader.readLine();
-
       // Ask the server the current version
       writer.println("INFO");
       writer.flush();
@@ -102,9 +113,6 @@ public class RouletteV1ClientImpl implements IRouletteV1Client
    @Override
    public String getProtocolVersion() throws IOException
    {
-      // Read the hello message from the server
-      reader.readLine();
-
       // Ask the server the current version
       writer.println("INFO");
       writer.flush();
