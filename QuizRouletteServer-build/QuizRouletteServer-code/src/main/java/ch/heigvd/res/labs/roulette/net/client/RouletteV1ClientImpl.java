a396e0c437a8e247387ecb5b9610beb840e4f492
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-03-23 17:45:25 +0100

    La beauté du code

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -9,18 +9,12 @@ import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Reader;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
+import java.nio.charset.Charset;
 import java.util.List;
-import java.util.Random;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -28,6 +22,8 @@ import java.util.logging.Logger;
  * 1).
  *
  * @author Olivier Liechti
+ * @author Rémi Jacquemard
+ * @author Aurélie Levy
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
@@ -37,100 +33,164 @@ public class RouletteV1ClientImpl implements IRouletteV1Client {
     private PrintWriter outputWriter;
     private BufferedReader inputReader;
 
-
+    /**
+     * Establishes a connection with the server, given its IP address or DNS
+     * name and its port.
+     *
+     * @param server the IP address or DNS name of the servr
+     * @param port the TCP port on which the server is listening
+     * @throws java.io.IOException
+     */
     @Override
     public void connect(String server, int port) throws IOException {
+        //The socket, writer and reader are initialized
         socket = new Socket(server, port);
-        outputWriter = new PrintWriter(socket.getOutputStream());
+        outputWriter = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
+                        socket.getOutputStream(), Charset.forName("UTF-8"))));        
+        inputReader = new BufferedReader(new InputStreamReader(
+                socket.getInputStream(), Charset.forName("UTF-8")));
 
-        inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        //Ignore la première ligne d'info reçue
+        //Ignore the first line received from the server
         inputReader.readLine();
-        System.out.println("-----------READED");
     }
 
+    /**
+     * Disconnects from the server by issuing the 'BYE' command.
+     *
+     * @throws IOException
+     */
     @Override
     public void disconnect() throws IOException {
         outputWriter.println(RouletteV1Protocol.CMD_BYE);
-        
+        outputWriter.flush();
+
+        //Close everything 
         outputWriter.close();
         inputReader.close();
         socket.close();
 
+        //Set to null everything, to keep a valid object state
         socket = null;
         outputWriter = null;
         inputReader = null;
     }
 
+    /**
+     * Checks if the client is connected with the server
+     *
+     * @return true if the client is connected with the server
+     */
     @Override
     public boolean isConnected() {
         return socket != null && socket.isConnected();
     }
 
+    /**
+     * Adds a student in the server database, by issuing the 'LOAD' command
+     *
+     * @param fullname the student's full name
+     * @throws IOException
+     */
     @Override
     public void loadStudent(String fullname) throws IOException {
+        //Ask the server to load the students
         outputWriter.println(RouletteV1Protocol.CMD_LOAD);
-        
+
+        //The student is written to the output
         outputWriter.println(fullname);
         outputWriter.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
         outputWriter.flush();
-        
-        //Ignore les infos du serveurs
+
+        //Ignore server's informations 
         inputReader.readLine(); //Info
         inputReader.readLine(); //Load done
     }
 
+    /**
+     * Adds a list of students in the server database, by issuing the 'LOAD'
+     * command
+     *
+     * @param students
+     * @throws IOException
+     */
     @Override
     public void loadStudents(List<Student> students) throws IOException {
+        //Ask the server to load the students
         outputWriter.println(RouletteV1Protocol.CMD_LOAD);
-        for (Student student : students) {
+        for (Student student : students) { //Write each of the students
             outputWriter.println(student.getFullname());
         }
         outputWriter.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
         outputWriter.flush();
-        
-        //Ignore les infos du serveurs
+
+        //Ignore server's informations 
         inputReader.readLine(); //Info
         inputReader.readLine(); //Load done        
     }
 
+    /**
+     * Asks the server to select a random student, by issuing the 'RANDOM'
+     * command and converting the result from json into a Student instance
+     *
+     * @return an instance of Student randomly selected by the server
+     * @throws IOException
+     */
     @Override
     public Student pickRandomStudent() throws EmptyStoreException, IOException {
+        //Ask for a random student
         outputWriter.println(RouletteV1Protocol.CMD_RANDOM);
         outputWriter.flush();
 
-        RandomCommandResponse response
-                = JsonObjectMapper.parseJson(inputReader.readLine(), RandomCommandResponse.class);
+        //Wait and parse the response
+        RandomCommandResponse response = JsonObjectMapper.parseJson(
+                inputReader.readLine(), RandomCommandResponse.class);
 
-        System.out.println("BLA ------------- " + response.getError());
-
+        //If the error is not empty, an error has occured
         if (!response.getError().isEmpty()) {
             throw new EmptyStoreException();
         }
-
+        
         return new Student(response.getFullname());
     }
 
+    /**
+     * Asks the server how many students are in the database by issuing the
+     * 'INFO' command
+     *
+     * @return the number of students in the database
+     * @throws IOException
+     */
     @Override
     public int getNumberOfStudents() throws IOException {
-        outputWriter.println(RouletteV1Protocol.CMD_INFO);        
+        //Ask for informations
+        outputWriter.println(RouletteV1Protocol.CMD_INFO);
         outputWriter.flush();
-        
-        InfoCommandResponse response
-                = JsonObjectMapper.parseJson(inputReader.readLine(), InfoCommandResponse.class);
 
+        //Wait and parse the response
+        InfoCommandResponse response = JsonObjectMapper.parseJson(
+                inputReader.readLine(), InfoCommandResponse.class);
+
+        //Return the number of students
         return response.getNumberOfStudents();
     }
 
+    /**
+     * Returns the protocol version implemented by the server
+     *
+     * @return the version of the Roulette Protocol
+     * @throws IOException
+     */
     @Override
     public String getProtocolVersion() throws IOException {
-        outputWriter.println(RouletteV1Protocol.CMD_INFO);        
+        //Ask for informations
+        outputWriter.println(RouletteV1Protocol.CMD_INFO);
         outputWriter.flush();
-        
+
+        //Wait and parse the response
         InfoCommandResponse response
                 = JsonObjectMapper.parseJson(inputReader.readLine(), InfoCommandResponse.class);
 
+        //Return the protocol version
         return response.getProtocolVersion();
     }
 
