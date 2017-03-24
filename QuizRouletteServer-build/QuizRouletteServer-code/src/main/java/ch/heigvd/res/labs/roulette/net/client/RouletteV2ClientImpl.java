e121b516c51db4b6c34f4927e00e90b028081e79
Newtt <m.chatelan@gmail.com>
2017-03-24 13:47:46 +0100

    Initial commit on branch

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -16,7 +16,7 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
    @Override
    public void clearDataStore() throws IOException
    {
-      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
    }
 
    @Override
