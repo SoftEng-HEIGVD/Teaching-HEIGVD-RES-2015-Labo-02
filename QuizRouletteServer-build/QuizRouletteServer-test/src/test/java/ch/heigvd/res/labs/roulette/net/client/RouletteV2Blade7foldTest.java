69e9e96cb678754403159fe70461f4a66a171f3a
BLADE7FOLD\Nathan <nathan.gonzalezmontes@heig-vd.ch>
2017-03-25 16:42:25 +0100

    Tests are done

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2Blade7foldTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2Blade7foldTest.java
@@ -6,7 +6,9 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.data.Student;
-import ch.heigvd.res.labs.roulette.net.client.RouletteV2ClientImpl;
+import ch.heigvd.res.labs.roulette.data.StudentsList;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import ch.heigvd.schoolpulse.TestAuthor;
 import java.io.IOException;
 import java.util.List;
@@ -16,6 +18,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -23,25 +27,9 @@ import static org.junit.Assert.*;
  */
 public class RouletteV2Blade7foldTest {
     
-    public RouletteV2Blade7foldTest() {
-    }
+    @Rule
+    public EphemeralClientServerPair roulettePair2 = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of clearDataStore method, of class RouletteV2ClientImpl.
      * @throws java.io.IOException
@@ -65,11 +53,24 @@ public class RouletteV2Blade7foldTest {
     public void testListStudents() throws IOException {
         System.out.println("List Students");
         RouletteV2ClientImpl instance = new RouletteV2ClientImpl();
-        List<Student> expResult = null;
+        StudentsList sl = new StudentsList();
+        List<Student> expResult = sl.getStudents();
         List<Student> result = instance.listStudents();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
+    /**
+     * Test of listStudents method, of class RouletteV2ClientImpl.
+     * @throws java.io.IOException
+     */
+    @Test
+    @TestAuthor(githubId = "Blade7fold")
+    public void testIfWeUseTheInfoCommandFromClientV2() throws IOException {
+        System.out.println("INFO Command used");
+        assertEquals(RouletteV2Protocol.VERSION, roulettePair2.getClient().getProtocolVersion());
+        RouletteV2ClientImpl usingINFO = new RouletteV2ClientImpl();
+        int nbOfStudents = roulettePair2.getClient().getNumberOfStudents();
+        int result = usingINFO.getNumberOfStudents();
+        assertEquals(nbOfStudents, result);
+    }
 }
