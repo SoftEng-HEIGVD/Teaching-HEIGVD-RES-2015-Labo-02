e6b3638959eab261454b28a6367d35aae8457700
Newtt <m.chatelan@gmail.com>
2017-03-25 17:21:19 +0100

    add the 2 response classes

---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/ByeCommandResponse.java
@@ -0,0 +1,10 @@
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+/**
+ * Created by matthieu on 3/25/17.
+ */
+public class ByeCommandResponse
+{
+   private String status;
+   private int numberOfCommands;
+}
---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/LoadCommandResponse.java
@@ -0,0 +1,10 @@
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+/**
+ * Created by matthieu on 3/25/17.
+ */
+public class LoadCommandResponse
+{
+   private String status;
+   private int numberOfStudents;
+}
