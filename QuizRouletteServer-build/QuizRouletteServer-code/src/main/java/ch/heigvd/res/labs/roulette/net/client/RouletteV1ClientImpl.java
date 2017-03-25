1c0a3a3aa7dab2b51de2a521285f936f97f13a6a
Nadir Benallal <nadir.benallal@heig-vd.ch>
2017-03-25 16:08:52 +0100

    Implementation of the client V1. All tests pass

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -7,64 +7,141 @@ import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * This class implements the client side of the protocol specification (version 1).
- * 
+ * This class implements the client side of the protocol specification (version
+ * 1).
+ *
  * @author Olivier Liechti
+ * @author Benallal Nadir and Verdasca Jimmy
  */
-public class RouletteV1ClientImpl implements IRouletteV1Client {
+public class RouletteV1ClientImpl implements IRouletteV1Client
+{
 
-  private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+    private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+    private Socket clientSocket;
+    private OutputStream os;
+    private InputStream is;
+    private BufferedReader responseBuffer;
+    private PrintWriter writer;
 
-  @Override
-  public void connect(String server, int port) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void connect(String server, int port) throws IOException
+    {
+        clientSocket = new Socket(server, port);
+        is = clientSocket.getInputStream();
+        os = clientSocket.getOutputStream();
 
-  @Override
-  public void disconnect() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+        responseBuffer = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        writer = new PrintWriter(new OutputStreamWriter(os, "UTF-8"));
+        
+        //We read the first answer from the server
+        responseBuffer.readLine();
+    }
 
-  @Override
-  public boolean isConnected() {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void disconnect() throws IOException
+    {
+        if (!clientSocket.isClosed())
+        {
+            clientSocket.close();
+        }
+    }
 
-  @Override
-  public void loadStudent(String fullname) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public boolean isConnected()
+    {
+        return clientSocket != null && clientSocket.isConnected();
+    }
 
-  @Override
-  public void loadStudents(List<Student> students) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void loadStudent(String fullname) throws IOException
+    {
+        List<Student> students = new LinkedList<>();
+        students.add(new Student(fullname));
+        loadStudents(students);
+    }
 
-  @Override
-  public Student pickRandomStudent() throws EmptyStoreException, IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public void loadStudents(List<Student> students) throws IOException
+    {
+        //Writing the student in the database
+        writer.println(RouletteV1Protocol.CMD_LOAD);
+        writer.flush();
 
-  @Override
-  public int getNumberOfStudents() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+        //Reading instructions message
+        responseBuffer.readLine();
 
-  @Override
-  public String getProtocolVersion() throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+        //Writing the data in the database
+        for (Student stud : students)
+        {
+            writer.println(stud.getFullname());
+            writer.flush();
+        }
 
+        //We have finished to send the data
+        writer.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+        writer.flush();
 
+        //Reading DATA LOADED
+        responseBuffer.readLine();
+    }
 
+    @Override
+    public Student pickRandomStudent() throws EmptyStoreException, IOException
+    {
+        writer.println(RouletteV1Protocol.CMD_RANDOM);
+        writer.flush();
+
+        //Getting the answer from the RANDOM command
+        String answer = responseBuffer.readLine();
+        RandomCommandResponse randomAnswer = JsonObjectMapper.parseJson(answer, RandomCommandResponse.class);
+        
+        String fullname = randomAnswer.getFullname();
+        
+        //If the database was empty, we must throw an Exception
+        if (!randomAnswer.getError().equals(""))
+            throw new EmptyStoreException();
+
+        return new Student(fullname);
+    }
+
+    @Override
+    public int getNumberOfStudents() throws IOException
+    {
+        return getInfos().getNumberOfStudents();
+    }
+
+    @Override
+    public String getProtocolVersion() throws IOException
+    {
+        return getInfos().getProtocolVersion();
+    }
+    
+    /**
+     * @brief Gets the main informations about the database
+     * @return the result from the INFO command sent to the server
+     * @throws IOException 
+     */
+    private InfoCommandResponse getInfos() throws IOException
+    {
+        writer.println(RouletteV1Protocol.CMD_INFO);
+        writer.flush();
+
+        String answer = responseBuffer.readLine();
+
+        return JsonObjectMapper.parseJson(answer, InfoCommandResponse.class);
+    }
 }
