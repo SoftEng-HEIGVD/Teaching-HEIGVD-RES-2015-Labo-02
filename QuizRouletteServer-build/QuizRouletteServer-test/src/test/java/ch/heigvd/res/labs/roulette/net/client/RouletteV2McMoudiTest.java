aa434c1da365ed6bf44791766ef0172831d5a0f9
McMoudi <mcmoudi@gmail.com>
2017-03-26 16:25:12 +0200

    McMoudi tests proper init

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2McMoudiTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2McMoudiTest.java
@@ -1,16 +1,22 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+import ch.heigvd.schoolpulse.TestAuthor;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import java.io.IOError;
+import java.io.IOException;
 
 /**
  * This class contains automated tests to validate the client and the server
- * implementation of the Roulette Protocol (version 1)
+ * implementation of the Roulette Protocol (version 2)
  *
  * @author Olivier Liechti
+ * @author Yann Mahmoudi
  */
-public class RouletteV2WasadigiTest {
+public class RouletteV2McMoudiTest {
 
   @Rule
   public ExpectedException exception = ExpectedException.none();
@@ -19,5 +25,11 @@ public class RouletteV2WasadigiTest {
   public EphemeralClientServerPair roulettePair = new EphemeralClientServerPair(RouletteV2Protocol.VERSION);
 
 
-  
+  @Test
+  @TestAuthor(githubId = "McMoudi")
+  public void plop() throws IOException {
+
+  }
+
+
 }
