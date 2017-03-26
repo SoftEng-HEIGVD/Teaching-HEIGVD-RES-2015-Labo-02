0f389e206cefad15161ceaa5f5fe92187bda3a86
qrzeller <pttptt3*>
2017-03-26 20:15:26 +0200

    adding some comments

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -24,8 +24,8 @@ import java.util.logging.Logger;
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
   private Socket socket = null;
-  PrintWriter w = null;
-  BufferedReader r = null;
+  private PrintWriter w = null;
+  private BufferedReader r = null;
 
   private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
 
@@ -64,7 +64,7 @@ public class RouletteV1ClientImpl implements IRouletteV1Client {
     r.readLine();//response
 
     w.println(fullname);//send student name
-    w.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+    w.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);//say we have finished
     w.flush();
 
     r.readLine();//DATA LOADED response
@@ -77,38 +77,41 @@ public class RouletteV1ClientImpl implements IRouletteV1Client {
     //send student one by one
     for (Student student: students
          ) {
-      w.println(student.getFullname());
+      w.println(student.getFullname());//print all students
     }
 
+    //Say we have finished
     w.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
     w.flush();
-    
+
     r.readLine();//data loaded response
   }
 
   @Override
   public Student pickRandomStudent() throws EmptyStoreException, IOException {
 
-    w.println(RouletteV1Protocol.CMD_RANDOM);w.flush();
+    w.println(RouletteV1Protocol.CMD_RANDOM);w.flush();//ask for RANDOM
+    //get the reponse and parse it as JSON
     RandomCommandResponse randomResponse = JsonObjectMapper.parseJson(r.readLine(),RandomCommandResponse.class);
 
-    if(!randomResponse.getError().isEmpty()) throw new EmptyStoreException();
+    if(!randomResponse.getError().isEmpty()) throw new EmptyStoreException();//be sur there is no error
 
     else
-      return new Student(randomResponse.getFullname());
+      return new Student(randomResponse.getFullname());//return a student object with his name
   }
 
   @Override
   public int getNumberOfStudents() throws IOException {
     w.println(RouletteV1Protocol.CMD_INFO);w.flush();
 
+    //use the InfoCommandResponse class to parse the number of Students. Because could vary in future version
     return JsonObjectMapper.parseJson(r.readLine(),InfoCommandResponse.class).getNumberOfStudents();
   }
 
   @Override
   public String getProtocolVersion() throws IOException {
     w.println(RouletteV1Protocol.CMD_INFO);w.flush();
-
+    //use of InfoCommandResponse class to parse the response and not be subject to future implementations
     return JsonObjectMapper.parseJson(r.readLine(),InfoCommandResponse.class).getProtocolVersion();
   }
 
