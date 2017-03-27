3514f9809768804d33834d39f60e3040c7423cf5
Gigon Quentin <gigon.quentin@gmail.com>
2017-03-27 21:27:14 +0200

    added one test

--- QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2QuentingigonTest.java
+++ QuizRouletteServer-build/QuizRouletteServer-test/src/test/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2QuentingigonTest.java
@@ -1,6 +1,7 @@
 package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
+import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import ch.heigvd.schoolpulse.TestAuthor;
 import org.junit.Ignore;
@@ -11,6 +12,7 @@ import org.junit.rules.ExpectedException;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * This class contains automated tests to validate the client and the server
@@ -63,6 +65,23 @@ public class RouletteV2QuentingigonTest {
         client.loadStudent("fabienne");
 
         assertEquals(client.listStudents().size(), client.getNumberOfStudents());
+    }
+
+    @Test
+    @TestAuthor(githubId = {"quentingigon"})
+    public void theServerShouldContainAllStudentsLoaded() throws IOException, EmptyStoreException {
+        IRouletteV2Client client = new RouletteV2ClientImpl();
+        int port = roulettePair.getServer().getPort();
+
+        client.connect("localhost", port);
+
+        client.loadStudent("sacha");
+        client.loadStudent("olivier");
+        client.loadStudent("fabienne");
+
+        assertTrue(client.listStudents().contains(new Student("sasha")));
+        assertTrue(client.listStudents().contains(new Student("olivier")));
+        assertTrue(client.listStudents().contains(new Student("fabienne")));
     }
 
     @Test
