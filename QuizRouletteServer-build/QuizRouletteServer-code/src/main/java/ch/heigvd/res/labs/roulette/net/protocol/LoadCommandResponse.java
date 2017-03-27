aef24909405212f9bbd084281e79b62fe88aac66
Christopher Meier <christopher.meier@heig-vd.ch>
2017-03-27 12:19:25 +0200

    Added LoadCommandResponse class. Updated port in RouletteV2Protocol.

---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/LoadCommandResponse.java
@@ -0,0 +1,36 @@
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+/**
+ * This class is used to serialize/deserialize the response sent by the server
+ * when processing the "LOAD" command defined in the protocol specification. The
+ * JsonObjectMapper utility class can use this class.
+ * 
+ * @author Christopher Meier
+ * @author Daniel Palumbo
+ */
+public class LoadCommandResponse {
+
+  private String status;
+  private int numberOfNewStudents;
+
+  public LoadCommandResponse() {
+    this.numberOfNewStudents = 0;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  public int getNumberOfNewStudents() {
+    return numberOfNewStudents;
+  }
+
+  public void setNumberOfNewStudents(int numberOfNewStudents) {
+    this.numberOfNewStudents = numberOfNewStudents;
+  }
+
+}
