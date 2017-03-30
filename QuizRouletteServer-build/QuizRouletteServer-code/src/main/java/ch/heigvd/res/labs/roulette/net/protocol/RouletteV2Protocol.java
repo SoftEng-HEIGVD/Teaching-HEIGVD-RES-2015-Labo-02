d5f3507abde2d8043f6d978ba5fb7020f551a761
Christopher Meier <christopher.meier@heig-vd.ch>
2017-03-30 18:59:50 +0200

    Added failure and success response.

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/RouletteV2Protocol.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/RouletteV2Protocol.java
@@ -16,6 +16,9 @@ public class RouletteV2Protocol extends RouletteV1Protocol {
 
   public static String RESPONSE_CLEAR_DONE = "DATASTORE CLEARED";
 
+  public static String RESPONSE_SUCCESS = "success";
+  public static String RESPONSE_FAILURE = "failure";
+
   public final static String[] SUPPORTED_COMMANDS = new String[]{CMD_HELP, CMD_RANDOM, CMD_LOAD, CMD_INFO, CMD_BYE, CMD_CLEAR, CMD_LIST};
 
 }
