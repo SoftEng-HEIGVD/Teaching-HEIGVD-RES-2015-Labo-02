78b46a32cd049967ffd1a81063a87b81b6a2ba39
Daniel Palumbo <dpalumbo06@gmail.com>
2017-03-30 16:50:06 +0200

    update endload

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -4,6 +4,7 @@ import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.LoadCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import java.io.IOException;
 import java.util.List;
@@ -40,6 +41,8 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
   }
 
   protected void endLoad() throws IOException {
+    // Read DATA LOADED message
+    super.endLoad();
     LoadCommandResponse lcr = JsonObjectMapper.parseJson(is.readLine(), LoadCommandResponse.class);
     if(lcr.getStatus().equalsIgnoreCase("success")) {
       LOG.log(Level.INFO, "Added successfully 0 students");
@@ -48,6 +51,5 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
       LOG.severe("Error, student not added !");
     }
   }
-
   
 }
