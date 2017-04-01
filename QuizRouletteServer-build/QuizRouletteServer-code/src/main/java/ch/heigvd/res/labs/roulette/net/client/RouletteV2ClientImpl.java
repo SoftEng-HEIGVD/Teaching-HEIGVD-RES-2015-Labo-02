d40cd51e1cd0c410764353fdec9d89b1d4d187ad
Lucas Elisei <faku99dev@gmail.com>
2017-04-01 18:57:37 +0200

    Revert "Merge branch 'fb-lab02-v2-tests' of github.com:SoftEng-HEIGVD/Teaching-HEIGVD-RES-2017-Labo-02 into fb-lab02-v2-app"
    This reverts commit 76f157c30e6f511e0623e6c2b1ae0418f02d743c, reversing
    changes made to e90df0c203a418f26ab794bcc3fde1416e583ea1.

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -4,38 +4,24 @@ import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.data.StudentsList;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
-
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * This class implements the client side of the protocol specification (version 2).
  *
- * @author Lucas ELISEI (faku99)
- * @author David TRUAN  (Daxidz)
+ * @author Olivier Liechti
  */
 public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRouletteV2Client {
 
-    private static final Logger LOG = Logger.getLogger(RouletteV2ClientImpl.class.getName());
+  @Override
+  public void clearDataStore() throws IOException {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 
-    @Override
-    public void clearDataStore() throws IOException {
-        writer.println(RouletteV2Protocol.CMD_CLEAR);
-        writer.flush();
-
-        LOG.info("RECEIVED: " + reader.readLine());
-    }
-
-    @Override
-    public List<Student> listStudents() throws IOException {
-        writer.println(RouletteV2Protocol.CMD_LIST);
-        writer.flush();
-
-        String response = reader.readLine();
-        LOG.info("RECEIVED: " + response);
-
-        return JsonObjectMapper.parseJson(response, StudentsList.class).getStudents();
-    }
-
+  @Override
+  public List<Student> listStudents() throws IOException {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+  
 }
