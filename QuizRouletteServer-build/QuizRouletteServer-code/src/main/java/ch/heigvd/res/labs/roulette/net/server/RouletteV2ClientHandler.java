3a627c3e9f900e1b5df336f826a6cd2378df262d
Edward Ransome <edward.ransome@heig-vd.ch>
2017-04-02 13:05:26 +0000

    avancé

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -26,7 +26,8 @@ import java.util.logging.Logger;
 public class RouletteV2ClientHandler implements IClientHandler {
 
   final static Logger LOG = Logger.getLogger(RouletteV2ClientHandler.class.getName());
-  static String status = "success";
+  static final String STATUS_SUCCESS = "success";
+  static final String STATUS_FAILURE = "failure";
 
 
   private final IStudentsStore store;
@@ -62,42 +63,54 @@ public class RouletteV2ClientHandler implements IClientHandler {
             rcResponse.setError("There is no student, you cannot pick a random one");
           }
           writer.println(JsonObjectMapper.toJson(rcResponse));
+          writer.flush();
           break;
         case RouletteV2Protocol.CMD_HELP:
           writer.println("Commands: " + Arrays.toString(RouletteV2Protocol.SUPPORTED_COMMANDS));
+          writer.flush();
           break;
         case RouletteV2Protocol.CMD_INFO:
           InfoCommandResponse responseInfo = new InfoCommandResponse(RouletteV2Protocol.VERSION, store.getNumberOfStudents());
           writer.println(JsonObjectMapper.toJson(responseInfo));
+          writer.flush();
           break;
         case RouletteV2Protocol.CMD_LOAD:
           writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
           writer.flush();
           int numberOfCurrentStudent = store.getNumberOfStudents();
-          store.importData(reader);
+          String status = STATUS_SUCCESS;
+          try{
+             store.importData(reader);
+          } catch (IOException e){
+              status = STATUS_FAILURE;
+          }
           LoadCommandResponse responseLoad = new LoadCommandResponse(status,store.getNumberOfStudents()-numberOfCurrentStudent);
           writer.println(responseLoad);
+          writer.flush();
           break;
         case RouletteV2Protocol.CMD_BYE:
           done = true;
-          ByeCommandResponse responseBye = new ByeCommandResponse(status,numberOfCommands);
+          ByeCommandResponse responseBye = new ByeCommandResponse(STATUS_SUCCESS, numberOfCommands);
           writer.println(JsonObjectMapper.toJson(responseBye));
+          writer.flush();
           break;
         case RouletteV2Protocol.CMD_CLEAR:
           store.clear();
           writer.println(RouletteV2Protocol.RESPONSE_CLEAR_DONE);
+          writer.flush();
           break;
 
         case RouletteV2Protocol.CMD_LIST:
           ListCommandResponse responseList = new ListCommandResponse(store.listStudents());
           writer.println(JsonObjectMapper.toJson(responseList));
+          writer.flush();
           break;
 
         default:
           writer.println("Huh? please use HELP if you don't know what commands are available.");
+          writer.flush();
           break;
       }
-      writer.flush(); //ici ou a chaque fois ?
     }
 
   }
