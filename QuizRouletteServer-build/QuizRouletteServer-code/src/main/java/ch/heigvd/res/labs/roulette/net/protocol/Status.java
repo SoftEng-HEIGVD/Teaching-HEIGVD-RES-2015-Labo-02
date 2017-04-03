102890160a067e585217232f660934f13bbe19d6
Sydney Hauke <sydney.hauke@gmail.com>
2017-04-03 21:56:15 +0200

    Comments on Status enum

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/Status.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/Status.java
@@ -2,6 +2,7 @@ package ch.heigvd.res.labs.roulette.net.protocol;
 
 /**
  * Created by sydney on 31.03.17.
+ * This class enumerates status values for command operations.
  */
 public enum Status {
     Success("success"),
