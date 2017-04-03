00348aecb48d043568adc50097e9f307a68b89d2
Loan Lassalle <loan.lassalle@heig-vd.ch>
2017-04-03 13:48:59 +0200

    Ajout de l'implémentation du Serveur et du Client V2.

---
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/protocol/ByeCommandResponse.java
@@ -0,0 +1,40 @@
+package ch.heigvd.res.labs.roulette.net.protocol;
+
+/**
+ * This class is used to serialize/deserialize the response sent by the server
+ * when processing the "BYE" command defined in the protocol specification. The
+ * JsonObjectMapper utility class can use this class.
+ *
+ * @author Loan Lassalle
+ * @author Tano Iannetta
+ */
+public class ByeCommandResponse {
+
+    private String status;
+    private int numberOfCommands;
+
+    public ByeCommandResponse() {
+
+    }
+
+    public ByeCommandResponse(String status, int numberOfCommands) {
+        this.status = status;
+        this.numberOfCommands = numberOfCommands;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus() {
+        this.status = status;
+    }
+
+    public int getNumberOfCommands() {
+        return numberOfCommands;
+    }
+
+    public void setNumberOfCommands() {
+        this.numberOfCommands = numberOfCommands;
+    }
+}
