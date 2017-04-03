220efccd4c8946af1689073b11b09f22d6a7a041
Michael Spierer <evondom@hotmail.com>
2017-04-03 17:21:21 +0200

    add methode to sendtoClient

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -28,7 +28,8 @@ public class RouletteV2ClientHandler implements IClientHandler {
   final static Logger LOG = Logger.getLogger(RouletteV2ClientHandler.class.getName());
   static final String STATUS_SUCCESS = "success";
   static final String STATUS_FAILURE = "failure";
-
+  BufferedReader reader;
+  PrintWriter writer;
 
   private final IStudentsStore store;
 
@@ -38,16 +39,20 @@ public class RouletteV2ClientHandler implements IClientHandler {
   public RouletteV2ClientHandler(IStudentsStore store) {
     this.store = store;
     numberOfCommands=0;
+     reader = new BufferedReader(new InputStreamReader(is));
+     writer = new PrintWriter(new OutputStreamWriter(os));
+  }
+
+  void sendToClient(String s){
+    writer.println(s);
+    writer.flush();
   }
 
 
   @Override
   public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-    PrintWriter writer = new PrintWriter(new OutputStreamWriter(os));
 
-    writer.println("Hello. Online HELP is available. Will you find it?");
-    writer.flush();
+    sendToClient("Hello. Online HELP is available. Will you find it?");
 
     String command;
     boolean done = false;
@@ -62,40 +67,41 @@ public class RouletteV2ClientHandler implements IClientHandler {
           } catch (EmptyStoreException ex) {
             rcResponse.setError("There is no student, you cannot pick a random one");
           }
-          writer.println(JsonObjectMapper.toJson(rcResponse));
-          writer.flush();
+          sendToClient(JsonObjectMapper.toJson(rcResponse));
+
           break;
         case RouletteV2Protocol.CMD_HELP:
-          writer.println("Commands: " + Arrays.toString(RouletteV2Protocol.SUPPORTED_COMMANDS));
-          writer.flush();
+          sendToClient("Commands: " + Arrays.toString(RouletteV2Protocol.SUPPORTED_COMMANDS));
           break;
         case RouletteV2Protocol.CMD_INFO:
           InfoCommandResponse responseInfo = new InfoCommandResponse(RouletteV2Protocol.VERSION, store.getNumberOfStudents());
-          writer.println(JsonObjectMapper.toJson(responseInfo));
-          writer.flush();
+
+          sendToClient(JsonObjectMapper.toJson(responseInfo));
           break;
         case RouletteV2Protocol.CMD_LOAD:
-          writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
-          writer.flush();
+          sendToClient(RouletteV2Protocol.RESPONSE_LOAD_START);
           int numberOfCurrentStudent = store.getNumberOfStudents();
           String status = STATUS_SUCCESS;
           try{
              store.importData(reader);
+            LoadCommandResponse responseLoad = new LoadCommandResponse(status,store.getNumberOfStudents()-numberOfCurrentStudent);
+            sendToClient(JsonObjectMapper.toJson(responseLoad));
           } catch (IOException e){
+            System.out.println("HELLO FAIL");
               status = STATUS_FAILURE;
           }
-          LoadCommandResponse responseLoad = new LoadCommandResponse(status,store.getNumberOfStudents()-numberOfCurrentStudent);
-          writer.println(responseLoad);
-          writer.flush();
+          System.out.println(status);
+
           break;
         case RouletteV2Protocol.CMD_BYE:
           done = true;
           ByeCommandResponse responseBye = new ByeCommandResponse(STATUS_SUCCESS, numberOfCommands);
-          writer.println(JsonObjectMapper.toJson(responseBye));
-          writer.flush();
+          sendToClient(JsonObjectMapper.toJson(responseBye));
+
           break;
         case RouletteV2Protocol.CMD_CLEAR:
           store.clear();
+          sendToClient();
           writer.println(RouletteV2Protocol.RESPONSE_CLEAR_DONE);
           writer.flush();
           break;
