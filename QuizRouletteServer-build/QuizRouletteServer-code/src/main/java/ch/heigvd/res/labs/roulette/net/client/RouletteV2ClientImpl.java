c1b197c88a453649e190047e8aea0d4a5aefb47e
Mathieu Monteverde <mathieu.monteverde@heig-vd.ch>
2017-04-03 12:07:08 +0000

    Completed the load method

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV2ClientImpl.java
@@ -2,7 +2,10 @@ package ch.heigvd.res.labs.roulette.net.client;
 
 import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
 import ch.heigvd.res.labs.roulette.data.Student;
+import ch.heigvd.res.labs.roulette.net.protocol.ByeCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.ListCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.LoadCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
 import java.io.IOException;
 import java.util.Arrays;
@@ -14,6 +17,9 @@ import java.util.List;
  * @author Olivier Liecht, modified by Mathieu Monteverde
  */
 public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRouletteV2Client {
+   
+   private ByeCommandResponse byeResponse = null;
+   private LoadCommandResponse loadResponse = null;
 
   @Override
   public void clearDataStore() throws IOException {
@@ -26,7 +32,8 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
   public void disconnect() throws IOException {
      sendDataToServer(RouletteV2Protocol.CMD_BYE);
      
-     String response = readServerResponse();
+     String result = readServerResponse();
+     byeResponse = JsonObjectMapper.parseJson(result, ByeCommandResponse.class);
      
      /*
      TODO: Read the server response
@@ -35,6 +42,44 @@ public class RouletteV2ClientImpl extends RouletteV1ClientImpl implements IRoule
      
      socket().close();
   }
+  
+  @Override
+   public void loadStudent(String fullname) throws IOException {
+      // Notify the server we are going to send him some data
+      sendDataToServer(RouletteV1Protocol.CMD_LOAD);
+      readServerResponse();
+      // Send Student fullname
+      sendDataToServer(fullname);
+      // Notify the server we ended transmitting data
+      sendDataToServer(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+      
+      String result = readServerResponse();
+      loadResponse = JsonObjectMapper.parseJson(result, LoadCommandResponse.class);
+      
+      // Do something with the response later ? 
+      // Nothing was implemented, but it would have been maybe by throwing an exception
+      // if not a success or something
+   }
+
+   @Override
+   public void loadStudents(List<Student> students) throws IOException {
+      // Notify the server we are going to send him some data
+      sendDataToServer(RouletteV1Protocol.CMD_LOAD);
+      readServerResponse();
+      
+      // Send all student information to the server
+      for (Student s : students) {
+         sendDataToServer(s.getFullname());
+      }
+      
+      // Notify the server we ended transmitting data
+      sendDataToServer(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER);
+      
+      String result = readServerResponse();
+      loadResponse = JsonObjectMapper.parseJson(result, LoadCommandResponse.class);
+      
+      // Same issue than with the single loadStudent method
+   }
 
   @Override
   public List<Student> listStudents() throws IOException {
