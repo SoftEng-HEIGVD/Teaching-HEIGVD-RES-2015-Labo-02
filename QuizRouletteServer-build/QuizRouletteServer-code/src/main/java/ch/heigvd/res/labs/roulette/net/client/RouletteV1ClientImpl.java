b07e1cde69bd65e708a5896ae78e162221facfd0
silver kameni <silverkameni@yahoo.fr>
2017-04-03 20:36:40 +0200

    lab02-all-test-passed

--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/client/RouletteV1ClientImpl.java
@@ -6,14 +6,12 @@ import ch.heigvd.res.labs.roulette.net.protocol.RouletteV1Protocol;
 import ch.heigvd.res.labs.roulette.data.Student;
 import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
 import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
-import java.util.Random;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,35 +20,36 @@ import java.util.logging.Logger;
  * This class implements the client side of the protocol specification (version
  * 1).
  *
- * @author silver kameni
+ * @author silver kameni & nguefack zacharie
  */
 public class RouletteV1ClientImpl implements IRouletteV1Client {
 
-   private static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
+    static final Logger LOG = Logger.getLogger(RouletteV1ClientImpl.class.getName());
 
     //create a client socket 
-   Socket myClientSocket;
+    Socket myClientSocket = null;
     // we need to create a boolean initialise on false to specify that we are connected
-   boolean connected = false;
+    boolean connected = false;
     //create a buffer reader
-   BufferedReader bf;
+    BufferedReader bf = null;
     // create an print writer;
-   PrintWriter pw;
-    //create a list of student to load all students 
-   List<Student> myStudentList = new ArrayList<>();
-
+    PrintWriter pw = null;
+   
     @Override
     public void connect(String server, int port) throws IOException {
         myClientSocket = new Socket(server, port);
         bf = new BufferedReader(new InputStreamReader(myClientSocket.getInputStream(), "UTF-8"));
         pw = new PrintWriter(new OutputStreamWriter(myClientSocket.getOutputStream(), "UTF-8"));
+        if((bf.readLine())!= null){ //read the first message og server
         connected = true;
-        bf.readLine(); // read the first message off server 
+        }
     }
 
     @Override
     public void disconnect() throws IOException {
         if (connected == true) {
+            pw.print(RouletteV1Protocol.CMD_BYE + "\n"); //signal end of connection
+            pw.flush();
             myClientSocket.close(); // close client socket
             connected = false;      // put the state of connection on false
             bf.close();
@@ -65,39 +64,53 @@ public class RouletteV1ClientImpl implements IRouletteV1Client {
 
     @Override
     public void loadStudent(String fullname) throws IOException {
-        if(!connected) return;
-        myStudentList.add(new Student(fullname)); // load each student to the list
-        loadStudents(myStudentList); // call loadStudents method and send our list
-
+        if (!connected) {
+            return;
+        }
+        pw.print(RouletteV1Protocol.CMD_LOAD + "\n");
+        pw.flush();
+        bf.readLine();       
+        pw.print(fullname + "\n");
+        pw.flush();
+        pw.print(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER + "\n");
+        pw.flush();
+        bf.readLine();
+       
     }
 
     @Override
     public void loadStudents(List<Student> students) throws IOException {
-         if(!connected) return;
-        if (students == null || students.isEmpty()) // return if our list is empty or not existing  
-        {
+        if (!connected) {
             return;
         }
-        pw.println(RouletteV1Protocol.CMD_LOAD); // signal begin of loading
+
+        pw.print(RouletteV1Protocol.CMD_LOAD + "\n"); // signal begin of loading
         pw.flush();
+        if (!bf.readLine().equalsIgnoreCase(RouletteV1Protocol.RESPONSE_LOAD_START)) {
+            LOG.log(Level.SEVERE, "not reply from server", RouletteV1Protocol.RESPONSE_LOAD_START);
+            return;
+        }
         // loading students
         for (Student myStudent : students) {
-            // print student fullname if it exist   
-            if (myStudent != null && !myStudent.getFullname().isEmpty()) {
-                pw.println(myStudent.getFullname());
-                pw.flush();
-            }
+            pw.println(myStudent.getFullname());
+            pw.flush();
         }
         pw.println(RouletteV1Protocol.CMD_LOAD_ENDOFDATA_MARKER); //signal end of loading
         pw.flush();
+        if (!bf.readLine().equalsIgnoreCase(RouletteV1Protocol.RESPONSE_LOAD_DONE)) {
+            LOG.log(Level.SEVERE, "not reply from server", RouletteV1Protocol.RESPONSE_LOAD_DONE);
+        }
     }
 
     @Override
     public Student pickRandomStudent() throws EmptyStoreException, IOException {
-        if(!connected) return null;
-        pw.println(RouletteV1Protocol.CMD_RANDOM);
+        if (!connected) {
+            return null;
+        }
+        pw.print(RouletteV1Protocol.CMD_RANDOM + "\n");
         pw.flush();
         RandomCommandResponse responseRandom = JsonObjectMapper.parseJson(bf.readLine(), RandomCommandResponse.class);
+        //throw an exception if error
         if (responseRandom.getError() != null) {
             throw new EmptyStoreException();
         }
@@ -106,18 +119,24 @@ public class RouletteV1ClientImpl implements IRouletteV1Client {
 
     @Override
     public int getNumberOfStudents() throws IOException {
-         
-        pw.println(RouletteV1Protocol.CMD_INFO);
+
+        if (!connected) {
+            return 0;
+        }
+        pw.print(RouletteV1Protocol.CMD_INFO + "\n");
         pw.flush();
-        return myStudentList.size(); // return size of our student list
+        return JsonObjectMapper.parseJson(bf.readLine(), InfoCommandResponse.class).getNumberOfStudents(); // return size of our student list
+
     }
 
     @Override
     public String getProtocolVersion() throws IOException {
-        
-        pw.print(RouletteV1Protocol.CMD_INFO);
+        if (!connected) {
+            return null;
+        }
+        pw.print(RouletteV1Protocol.CMD_INFO + "\n");
         pw.flush();
-        return RouletteV1Protocol.VERSION; // return version of our protocol
+        return JsonObjectMapper.parseJson(bf.readLine(), InfoCommandResponse.class).getProtocolVersion(); // return version of our protocol
     }
 
 }
--- QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
+++ QuizRouletteServer-build/QuizRouletteServer-code/src/main/java/ch/heigvd/res/labs/roulette/net/server/RouletteV2ClientHandler.java
@@ -1,24 +1,125 @@
 package ch.heigvd.res.labs.roulette.net.server;
 
+import ch.heigvd.res.labs.roulette.data.EmptyStoreException;
 import ch.heigvd.res.labs.roulette.data.IStudentsStore;
+import ch.heigvd.res.labs.roulette.data.JsonObjectMapper;
+import ch.heigvd.res.labs.roulette.data.StudentsList;
+import ch.heigvd.res.labs.roulette.net.protocol.ByeCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.InfoCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.LoadCommandResponse;
+import ch.heigvd.res.labs.roulette.net.protocol.RandomCommandResponse;
+
+import ch.heigvd.res.labs.roulette.net.protocol.RouletteV2Protocol;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This class implements the Roulette protocol (version 2).
  *
- * @author Olivier Liechti
+ * @author silver kameni & nguefack zacharie
  */
 public class RouletteV2ClientHandler implements IClientHandler {
 
-  public RouletteV2ClientHandler(IStudentsStore store) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of gen  
-  }
+    final static Logger LOG = Logger.getLogger(RouletteV2ClientHandler.class.getName());
+    private final IStudentsStore store;
+    private int numberOfCommands; // variable use to count number of command executed
 
-  @Override
-  public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    public RouletteV2ClientHandler(IStudentsStore store) {
+        this.store = store;
+    }
+
+    @Override
+    public void handleClientConnection(InputStream is, OutputStream os) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(os));
+
+        writer.println("Hello. Online HELP is available. Will you find it?");
+        writer.flush();
+
+        String command;
+        boolean done = false;
+        while (!done && ((command = reader.readLine()) != null)) {
+            LOG.log(Level.INFO, "COMMAND: {0}", command);
+
+            switch (command.toUpperCase()) {
+                
+                case RouletteV2Protocol.CMD_RANDOM:
+                    ++numberOfCommands;
+                    RandomCommandResponse rcResponse = new RandomCommandResponse();
+                    try {
+                        rcResponse.setFullname(store.pickRandomStudent().getFullname());
+                    } catch (EmptyStoreException ex) {
+                        rcResponse.setError("There is no student, you cannot pick a random one");
+                    }
+                    writer.println(JsonObjectMapper.toJson(rcResponse));
+                    writer.flush();
+                    break;
+                    
+                case RouletteV2Protocol.CMD_HELP:
+                    ++numberOfCommands;
+                    writer.println("Commands: " + Arrays.toString(RouletteV2Protocol.SUPPORTED_COMMANDS));
+                    break;
+                    
+                case RouletteV2Protocol.CMD_INFO:
+                    ++numberOfCommands;
+                    InfoCommandResponse response = new InfoCommandResponse(RouletteV2Protocol.VERSION, store.getNumberOfStudents());
+                    writer.println(JsonObjectMapper.toJson(response));
+                    writer.flush();
+                    break;
+                    
+                case RouletteV2Protocol.CMD_LIST:
+                    ++numberOfCommands;
+                    StudentsList studentsList = new StudentsList();
+                    studentsList.setStudents(store.listStudents());
+                    writer.println(JsonObjectMapper.toJson(studentsList));
+                    writer.flush();
+                    break;
+
+                case RouletteV2Protocol.CMD_CLEAR:
+                    ++numberOfCommands;
+                    store.clear();
+                    writer.println(RouletteV2Protocol.RESPONSE_CLEAR_DONE);
+                    writer.flush();
+                    break;
+                    
+                case RouletteV2Protocol.CMD_LOAD:
+                    ++numberOfCommands;
+                    writer.println(RouletteV2Protocol.RESPONSE_LOAD_START);
+                    writer.flush();
+                    int NumberOfStudentsBefore = store.getNumberOfStudents();
+                    store.importData(reader);
+                    int NumberOfNewStudent = store.getNumberOfStudents() - NumberOfStudentsBefore;
+                    LoadCommandResponse loadCommandResponse = new LoadCommandResponse("success", NumberOfNewStudent);
+                    writer.println(JsonObjectMapper.toJson(loadCommandResponse));
+                    writer.flush();
+                    break;
+                    
+                case RouletteV2Protocol.CMD_BYE:
+                    ++numberOfCommands;
+                    ByeCommandResponse byeCommandResponse = new ByeCommandResponse("success", numberOfCommands);
+                    writer.println(JsonObjectMapper.toJson(byeCommandResponse));
+                    writer.flush();
+                    done = true;
+                    break;
+                    
+                default:
+                    writer.println("Huh? please use HELP if you don't know what commands are available.");
+                    writer.flush();
+                    break;
+            }
+            writer.flush();
+        }
+
+    }
 
 }
